3bfc986cb505c59589d4ed222db0e0a6470e3dd8
Nelson Lin <nelsonlin0321@outlook.com.>
2025-11-17 20:50:21 +0800

    feat: add database and logging dependencies
    - Add loguru for logging functionality
    - Add upstash-redis for Redis integration
    - Add type stubs for pytz and tqdm
    - Create new db.py module with Prisma and Redis setup

---
+++ src/db.py
@@ -0,0 +1,28 @@
+from loguru import logger
+from upstash_redis.asyncio import Redis
+from prisma import Prisma
+from prisma.engine.errors import AlreadyConnectedError, NotConnectedError
+from src.utils import get_env
+
+
+prisma = Prisma(auto_register=True)
+
+
+async def connect():
+    try:
+        await prisma.connect()
+    except AlreadyConnectedError:
+        logger.info("Already connected to Prisma")
+    finally:
+        return prisma
+
+
+async def disconnect():
+    try:
+        await prisma.disconnect()
+    except NotConnectedError:
+        pass
+
+
+redis = Redis(url=get_env("UPSTASH_REDIS_REST_URL"),
+              token=get_env("UPSTASH_REDIS_REST_TOKEN"))
